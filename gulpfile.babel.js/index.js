67b281e0f7036b6272d933db2e5b4f1edbe6c5cf
Johan Mouchet <mouchet.johan@gmail.com>
2019-04-23 22:56:59 +1000

    Fix in gulp-cli fixed an issue with ES6 tasks exports

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -4,18 +4,12 @@
  * To list available tasks, try running: gulp --tasks
  */
 
-import { task } from 'gulp';
-
 import lintcss from './tasks/lintcss';
 import dev from './tasks/dev';
 import prod from './tasks/prod';
 
-export const taskLintcss = task(lintcss);
-export const taskDev = task(dev);
-export const taskProd = task(prod);
+export { lintcss };
+export { dev };
+export { prod };
 
 export { default } from './tasks/dev';
-
-// TODO: Pending merge of "Fix Honor displayName in ESM exports":
-//      https://github.com/gulpjs/gulp-cli/pull/189
-//      export { buildcss };
