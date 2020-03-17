142606ee7d5c51ee568856cf6838917db6155b97
Johan Mouchet <mouchet.johan@gmail.com>
2020-03-17 17:32:03 +1100

    Updated GitHub workflow

--- gulpfile.babel.js/tasks/dist.js
+++ gulpfile.babel.js/tasks/dist.js
@@ -3,6 +3,6 @@ import buildcss from "../utils/buildcss";
 const dist = done => buildcss("dist")(done);
 
 dist.displayName = "dist";
-dist.description = "Build Fluxgrid and debug CSS";
+dist.description = "Build Fluxgrid, debug and demo CSS";
 
 export default dist;
