5b657cfb5dac8f96efe570de156fc72012513aff
Johan Mouchet <mouchet.johan@gmail.com>
2020-08-12 09:45:22 +1000

    Migrated to Parcel v2, requires deleting package-lock.json,  node_modules and re installing dependencies

---
+++ docs/index.36bba811.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".toggle-grid-debug-checkbox").addEventListener("change",(function(){return e()}));var e=function(){return document.body.classList.toggle("fluxgrid-debug")}}();
+//# sourceMappingURL=index.36bba811.js.map
