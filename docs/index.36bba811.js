60cfbc82c730e44db56ec211ccc889dcf6c9ddff
Johan Mouchet <mouchet.johan@gmail.com>
2020-08-12 10:12:35 +1000

    Revert "Migrated to Parcel v2, requires deleting package-lock.json,  node_modules and re installing dependencies"
    This reverts commit 5b657cfb5dac8f96efe570de156fc72012513aff.

--- docs/index.36bba811.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".toggle-grid-debug-checkbox").addEventListener("change",(function(){return e()}));var e=function(){return document.body.classList.toggle("fluxgrid-debug")}}();
-//# sourceMappingURL=index.36bba811.js.map
